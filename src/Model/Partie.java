512dfba9facedb6d838b36584567753296c4b5ad
vicou96 <vicou96@gmail.com>
2015-11-20 10:25:00 +0100

    Update Partie.java
    ajout du void :P

--- src/Model/Partie.java
+++ src/Model/Partie.java
@@ -22,7 +22,7 @@ public class Partie {
 		deckRessource=new DeckRessource();
 		deckDeveloppement=new DeckDeveloppement();
 	}
-	public initMainJoueur(Joueur j)
+	public void initMainJoueur(Joueur j)
 	{
 		//TODO récupérer les cartes dans le deck ressource et ajouter le bon nombre de carte dans la main du joueur j
 	}
