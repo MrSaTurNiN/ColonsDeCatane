da4d08f91c4b972b59fdd9e7bc252e73bf95d304
Nostradadamus <adam.smoothie@hotmail.fr>
2016-03-09 21:32:10 +0100

    Deuxième commit pour le commerce, cette fois, il y a deux boutons sur les raccourcis ! C'est plutôt cool lel

--- src/views/Modules/ModuleBarRaccourcis.java
+++ src/views/Modules/ModuleBarRaccourcis.java
@@ -20,11 +20,12 @@ public class ModuleBarRaccourcis extends Module implements ViewConstants {
 
     private Image background_bouton;
     private Image icone_banque;
+    private Image icone_commerce;
 
     public Button btn;
 
-    private ImageView background_boutons[] = new ImageView[1];
-    private ImageView icones[] = new ImageView[1];
+    private ImageView background_boutons[] = new ImageView[2];
+    private ImageView icones[] = new ImageView[2];
 
     public ModuleBarRaccourcis(Partie p, Group r, Stage s)
     {
@@ -39,6 +40,7 @@ public class ModuleBarRaccourcis extends Module implements ViewConstants {
     public void  initImage() {
         background_bouton = new Image(getClass().getResourceAsStream("/assets/img/background_bouton.png"));
         icone_banque = new Image(getClass().getResourceAsStream("/assets/img/icone_banque.png"));
+        icone_commerce = new Image(getClass().getResourceAsStream("/assets/img/icone_commerce.png"));
 
     }
 
@@ -48,16 +50,18 @@ public class ModuleBarRaccourcis extends Module implements ViewConstants {
         root.getChildren().remove(background_boutons[0]);
         root.getChildren().remove(icones[0]);
         drawBoutons();
-        root.getChildren().add(background_boutons[0]);
-        root.getChildren().add(icones[0]);
+        root.getChildren().addAll(background_boutons);
+        root.getChildren().addAll(icones);
     }
 
     public void drawBoutons()
     {
         background_boutons[0] = new ImageView();
+        background_boutons[1] = new ImageView();
         icones[0] = new ImageView();
+        icones[1] = new ImageView();
         drawButtonCenter(background_boutons[0], icones[0], background_bouton, icone_banque, stage.getWidth()- 80, stage.getHeight() - 100, "button_bank");
-        //drawButtonCenter(background_boutons[0], icones[0], background_bouton, icone_banque, stage.getWidth()- 120, stage.getHeight() - 100, "button_trade");
+        drawButtonCenter(background_boutons[1], icones[1], background_bouton, icone_commerce, stage.getWidth()- 150, stage.getHeight() - 100, "button_trade");
 
 
         //btn.setStyle("-fx-background-color: transparent;");
